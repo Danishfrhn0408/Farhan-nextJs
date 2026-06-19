0539a0cff3bde4b72497ba4d957854705f341b47
Danishfrhn0408 <109861232+Danishfrhn0408@users.noreply.github.com>
2026-06-19 17:31:26 +0800

    update

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -111,4 +111,319 @@ const MyPage = () => {
                             initial={{ x: -100, opacity: 0 }}
                             whileInView={{ x: 0, opacity: 1 }}
                             transition={{
-                                delay:
+                                delay: 0.4,
+                                type: "spring",
+                            }}>
+                            Hi! I&rsquo;m Farhan, a software developer with a strong focus on Artificial Intelligence and Computer Vision. I am passionate about building smart systems, integrating hardware with IoT, and developing automated data pipelines.
+                        </motion.p>
+                        <motion.div
+                            className="buttons flex flex-row justify-center items-center space-x-4 mt-10"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.5,
+                                type: "spring",
+                            }}>
+                            <Button variation="primary">
+                                <Link
+                                    href={"/docs/cv.pdf"}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    download>
+                                    Download CV
+                                </Link>
+                            </Button>
+                            <Button variation="secondary">
+                                <a href="#contact">Contact Me</a>
+                            </Button>
+                        </motion.div>
+                    </motion.div>
+                    <motion.div
+                        className="hidden md:flex col-span-1 mx-auto justify-center items-center "
+                        initial={{ x: 100, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{
+                            delay: 0.7,
+                            type: "spring",
+                        }}>
+                        <div className="rounded-full h-auto w-auto max-w-[26vw] lg:px-12 grayscale hover:grayscale-0 transition-all ease duration-300">
+                            <Image
+                                src={Me}
+                                width={400}
+                                height={550}
+                                placeholder="blur"
+                                alt="Farhan"
+                                className="rounded-full w-full h-full object-cover"
+                            />
+                        </div>
+                    </motion.div>
+                </div>
+            </Section>
+            <Section>
+                <div className="relative md:h-screen w-screen gap-4 flex justify-center items-center flex-col overflow-hidden">
+                    <div className="z-0 mb-48 md:mb-0  md:absolute md:top-1/2  md:right-[10%] md:-translate-y-1/2">
+                        <motion.div
+                            className="relative bg-slate-300 rounded-sm h-[400px] md:h-[60vh] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0"
+                            initial={{
+                                x: 300,
+                                opacity: 0,
+                                z: -100,
+                            }}
+                            whileInView={{
+                                x: 0,
+                                opacity: 1,
+                                z: 0,
+                            }}
+                            transition={{
+                                delay: 0.5,
+                                type: "spring",
+                                stiffness: 100,
+                                damping: 20,
+                            }}>
+                            <Image
+                                src={MeAbout}
+                                fill
+                                sizes="(max-width: 768px) 80vw, 30vw"
+                                className="object-cover"
+                                alt="About Farhan"
+                                placeholder="blur"
+                            />
+                        </motion.div>
+                    </div>
+                    <div className="z-10 w-full absolute md:w-auto  md:left-[10%] top-[60%] md:top-1/3 col-span-2 flex flex-col justify-center items-start md:items-start text-start px-10 py-5">
+                        <motion.h1
+                            className="bg-white lg:bg-transparent bg-opacity-50 px-3 md-px-0 text-black text-5xl md:text-8xl font-bold"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.1,
+                                type: "spring",
+                            }}>
+                            About Me
+                        </motion.h1>
+                        <Hr />
+                        <motion.p
+                            className="title  text-xl mt-4 tracking-wider text-gray-500 leading-[1.7rem] mb-5"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.2,
+                                type: "spring",
+                            }}>
+                            A brief introduction to my technical background and passion.
+                        </motion.p>
+                        <motion.div
+                            initial={{ y: 40, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.3,
+                                type: "spring",
+                            }}>
+                            <Button variation="primary">
+                                <Link href="/about">Learn More</Link>
+                            </Button>
+                        </motion.div>
+                    </div>
+                </div>
+            </Section>
+            <Section>
+                <div className="relative md:h-screen w-screen gap-4 p-10 flex justify-center items-center flex-col overflow-hidden">
+                    <div className="z-0 mb-48 md:mb-0  md:absolute md:top-1/2  md:right-[10%] md:-translate-y-1/2">
+                        <motion.div
+                            className="relative bg-slate-300 rounded-sm h-[400px] md:h-[60vh] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0"
+                            initial={{
+                                x: 300,
+                                opacity: 0,
+                                z: -100,
+                            }}
+                            whileInView={{
+                                x: 0,
+                                opacity: 1,
+                                z: 0,
+                            }}
+                            transition={{
+                                delay: 0.5,
+                                type: "spring",
+                                stiffness: 100,
+                                damping: 20,
+                            }}>
+                            <Image
+                                src={ProjectAll}
+                                fill
+                                sizes="(max-width: 768px) 80vw, 30vw"
+                                className="object-cover"
+                                alt="Projects"
+                                placeholder="blur"
+                            />
+                        </motion.div>
+                    </div>
+                    <div className="z-10 w-full absolute md:w-auto  md:left-[10%] top-[60%] md:top-1/3 col-span-2 flex flex-col justify-center items-start md:items-start text-start px-10 py-5">
+                        <motion.h1
+                            className="bg-white lg:bg-transparent bg-opacity-50 px-3 md-px-0 text-black text-5xl md:text-8xl font-bold"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.1,
+                                type: "spring",
+                            }}>
+                            My Projects
+                        </motion.h1>
+                        <Hr />
+                        <motion.p
+                            className="title  text-xl mt-4 tracking-wider text-gray-500 leading-[1.7rem] mb-5"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.2,
+                                type: "spring",
+                            }}>
+                            Selected works that I&apos;ve built, focusing on AI and systems architecture.
+                            <span className="bg-transparent md:bg-gray-100 bg-opacity-50 xl:bg-transparent">
+                                {" "}
+                                Including my final year projects.
+                            </span>
+                        </motion.p>
+                        <motion.div
+                            initial={{ y: 40, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.3,
+                                type: "spring",
+                            }}>
+                            <Button variation="primary">
+                                <Link href="/projects">View Projects</Link>
+                            </Button>
+                        </motion.div>
+                    </div>
+                </div>
+            </Section>
+            <Section>
+                <div className="relative md:h-screen w-screen  gap-4 p-10 flex justify-center items-center flex-col overflow-hidden">
+                    <div className="z-0 mb-48 md:mb-0  md:absolute md:top-1/2  md:right-[10%] md:-translate-y-1/2">
+                        <motion.div
+                            className="relative bg-slate-300 rounded-sm h-[400px] md:h-[60vh] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0"
+                            initial={{
+                                x: 300,
+                                opacity: 0,
+                                z: -100,
+                            }}
+                            whileInView={{
+                                x: 0,
+                                opacity: 1,
+                                z: 0,
+                            }}
+                            transition={{
+                                delay: 0.5,
+                                type: "spring",
+                                stiffness: 100,
+                                damping: 20,
+                            }}>
+                            <Image
+                                src={Setup}
+                                fill
+                                sizes="(max-width: 768px) 80vw, 30vw"
+                                className="object-cover"
+                                alt="Setup"
+                                placeholder="blur"
+                            />
+                        </motion.div>
+                    </div>
+                    <div className="z-10 w-full absolute md:w-auto  md:left-[10%] top-[60%] md:top-1/3 col-span-2 flex flex-col justify-center items-start md:items-start text-start px-10 overflow-hidden">
+                        <motion.h1
+                            className="bg-white lg:bg-transparent bg-opacity-50 px-3 md-px-0 text-black text-5xl md:text-8xl font-bold mb-3"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.1,
+                                type: "spring",
+                            }}>
+                            Get In Touch
+                        </motion.h1>
+                        <Hr />
+                        <motion.p
+                            className="title text-xl mt-4 tracking-wider text-gray-500 leading-[1.7rem] md:mb-5"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.2,
+                                type: "spring",
+                            }}>
+                            Feel free to contact me if you have any{" "}
+                            <span className="bg-transparent md:bg-gray-100 bg-opacity-50 xl:bg-transparent">
+                                questions or opportunities to discuss.
+                            </span>
+                        </motion.p>
+                        <motion.p
+                            className="title text-xl mt-4 tracking-wider text-gray-500 leading-[1.7rem] mb-5"
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.3,
+                                type: "spring",
+                            }}>
+                            <a href="mailto:your.email@gmail.com?subject=Hello&body=Hello Farhan,">
+                                your.email@gmail.com
+                            </a>
+                        </motion.p>
+                        {/* icons */}
+                        <div className="flex justify-center items-center space-x-4">
+                            <motion.a
+                                href="mailto:your.email@gmail.com?subject=Hello&body=Hello Farhan,"
+                                aria-label="Send email"
+                                className="flex justify-center items-center bg-gray-700 w-14 h-14 rounded-full text-gray-100 hover:bg-gray-400 transition-all ease-in-out duration-300"
+                                initial={{ y: 40, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                transition={{
+                                    y: { delay: 0.1 },
+                                    opacity: { delay: 0.2 },
+                                }}>
+                                <FontAwesomeIcon
+                                    icon={faEnvelope}
+                                    className="text-3xl"
+                                />
+                            </motion.a>
+
+                            <motion.a
+                                href="https://github.com/YourGitHubUsername"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label="GitHub profile"
+                                className="flex justify-center items-center bg-gray-700 w-14 h-14 rounded-full text-gray-100 hover:bg-gray-400 transition-all ease-in-out duration-300"
+                                initial={{ opacity: 0, y: 40 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    y: { delay: 0.2 },
+                                    opacity: { delay: 0.3 },
+                                }}>
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    className="text-3xl"
+                                />
+                            </motion.a>
+                            <motion.a
+                                href="https://www.linkedin.com/in/YourLinkedInUsername/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label="LinkedIn profile"
+                                className="flex justify-center items-center bg-gray-700 w-14 h-14 rounded-full text-gray-100 hover:bg-gray-400 transition-all ease-in-out duration-300"
+                                initial={{ opacity: 0, y: 40 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    y: { delay: 0.4 },
+                                    opacity: { delay: 0.5 },
+                                }}>
+                                <FontAwesomeIcon
+                                    icon={faLinkedin}
+                                    className="text-3xl"
+                                />
+                            </motion.a>
+                        </div>
+                    </div>
+                </div>
+            </Section>
+            <ScrollIndicator />
+        </FullPageWrapper>
+    );
+};
+
+export default MyPage;
